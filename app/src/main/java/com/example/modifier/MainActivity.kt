21f7a47cc22dd1c3230d42fcd2569d4e626dcff6
Monu <105378383+PROGRAMMINGKING8055@users.noreply.github.com>
2023-09-30 23:00:04 +0530

    init commit

--- app/src/main/java/com/example/modifier/MainActivity.kt
+++ app/src/main/java/com/example/modifier/MainActivity.kt
@@ -1,50 +1,58 @@
 package com.example.modifier
 
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.shape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            Column(
-//                modifier = Modifier
-//                    .background(color = Color.Green)
-//                    .padding(20.dp).fillMaxWidth(1f)
-//            ) {
-//                Text(text = "Hello")
-//                Text(text = "World")
-//            }
+            layout()
 
-        Column {
-            Button(onClick = {
-                Toast.makeText(this@MainActivity,"Hello Monu",Toast.LENGTH_LONG).show()
-
-
-            }) {
-                Text(text = "Hello")
-
-
-            }
         }
-        }
-
     }
 
+    @Composable
+    fun layout() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Red)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
 
+        ) {
+            for (i in 1..5) {
+                Text(text = "Monu Yadav", fontSize = 28.sp)
+            }
+        }
+    }
 }
